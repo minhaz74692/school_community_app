f55c1eb68322688d282fdf91d8890d5a66ba552c
minhaz74692 <minhaz.ipe.sust@gmail.com>
2024-05-15 23:43:59 +0530

    change in homepage

--- lib/configs/app_config.dart
+++ lib/configs/app_config.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppConfig {
   static Color? appThemeColor = Colors.blueAccent;
+
+  static String appIcon = "assets/images/icon.png";
 }
--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,8 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:sust_app/configs/app_config.dart';
 import 'package:sust_app/constants/app_constants.dart';
 import 'package:sust_app/utills/custom_elevated_button.dart';
 import 'package:sust_app/utills/custom_text_field.dart';
+import 'package:sust_app/utills/theme/text.style.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
@@ -63,50 +67,82 @@ class _HomeTabState extends State<HomeTab> {
     }
 
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Row(
           children: [
-            ElevatedButton(
-              onPressed: () {
-                createCustomersCollection()
-                    .then((value) => debugPrint("succcess"));
-              },
-              child: Text("Click"),
+            Image.asset(
+              AppConfig.appIcon,
+              width: 50,
             ),
-            ElevatedButton(
-              onPressed: () {
-                AppConstants.hideNavBar(false, context);
-              },
-              child: Text("Show"),
+            Text(
+              "SUST Community",
+              style: interStyle18_600,
             ),
-            ElevatedButton(
-              onPressed: () {
-                AppConstants.hideNavBar(true, context);
-              },
-              child: Text("Hide"),
-            ),
-            CustomTextField(
-              controller: nameController,
-            ),
-            CustomElevatedButton(
-              title: "Save",
-              onPressed: () {
-                setState(() {
-                  userName = nameController.text.toString();
-                });
-                createCustomersCollection()
-                    .then((value) => debugPrint("succcess"));
-                debugPrint(nameController.text.toString());
-              },
-            ),
-            Container(
-              height: 100,
-              width: 200,
-              color: Theme.of(context).primaryColor,
-            ),
-            
           ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.share,
+              color: Colors.grey[700],
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                child: Text(
+                  "Shahjalal University of Science and Technology (SUST) was established in 1986. The only university of its kind at that time, it started it''s journey on the 13th of February 1991 with only three departments: Physics, Chemistry and Economics, 13 teachers and 205 students. It has now expanded to 7 schools, 27 departments , 2 institutes and and centers. The number of teachers has grown to 566 and the students to 8596. Besides, the University has 12 affiliated colleges under the School of Medical Sciences with 4000 students.",
+                  style: interStyle14_400,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  createCustomersCollection()
+                      .then((value) => debugPrint("succcess"));
+                },
+                child: Text("Click"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  AppConstants.hideNavBar(false, context);
+                },
+                child: Text("Show"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  AppConstants.hideNavBar(true, context);
+                },
+                child: Text("Hide"),
+              ),
+              CustomTextField(
+                controller: nameController,
+              ),
+              CustomElevatedButton(
+                title: "Save",
+                onPressed: () {
+                  setState(() {
+                    userName = nameController.text.toString();
+                  });
+                  createCustomersCollection()
+                      .then((value) => debugPrint("succcess"));
+                  debugPrint(nameController.text.toString());
+                },
+              ),
+              Container(
+                height: 100,
+                width: 200,
+                color: Theme.of(context).primaryColor,
+              ),
+            ],
+          ),
         ),
       ),
     );
